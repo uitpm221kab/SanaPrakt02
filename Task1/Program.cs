d878645b5550650ce14ffea5abceb8d411f9bba5
Anatoliy Kolesnyk <uitpm221_kab@student.ztu.edu.ua>
2022-11-10 20:27:36 +0200

    Changed last 2 lines of output

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,5 +3,5 @@ Console.WriteLine("Довільне");
 Console.WriteLine("Довільне");
 Console.WriteLine("Довільне");
 Console.WriteLine("Курси Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Commerce Software Development Bootcamp");
+Console.WriteLine("Sample text");
+Console.WriteLine("Sample text");
