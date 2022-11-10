42f6273259602fd830a76c2afe88cf5b112accb4
Anatoliy Kolesnyk <uitpm221_kab@student.ztu.edu.ua>
2022-11-10 20:23:18 +0200

    Output duplicated  5 times

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Курси Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Commerce Software Development Bootcamp");
